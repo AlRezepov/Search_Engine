4103e3881b79e9d6ecf3706942339143ed0c3491
Aleksey <alexrezepovjr@mail.ru>
2024-07-30 00:13:20 +1000

    Update_Database_files

--- database/database.cpp
+++ database/database.cpp
@@ -7,3 +7,20 @@ Database::Database(const Config& config)
         " user=" + config.db_user +
         " password=" + config.db_password) {
 }
+
+void Database::save_document(const std::string& url, const std::string& content, pqxx::work& txn) {
+    txn.exec_params("INSERT INTO search_engine.documents (url, content) VALUES ($1, $2) ON CONFLICT (url) DO NOTHING", url, content);
+}
+
+void Database::save_word_frequency(int document_id, const std::string& word, int frequency, pqxx::work& txn) {
+    pqxx::result r = txn.exec_params("SELECT id FROM search_engine.words WHERE word = $1", word);
+    int word_id;
+    if (r.empty()) {
+        r = txn.exec_params("INSERT INTO search_engine.words (word) VALUES ($1) RETURNING id", word);
+        word_id = r[0][0].as<int>();
+    }
+    else {
+        word_id = r[0][0].as<int>();
+    }
+    txn.exec_params("INSERT INTO search_engine.word_frequencies (document_id, word_id, frequency) VALUES ($1, $2, $3) ON CONFLICT (document_id, word_id) DO NOTHING", document_id, word_id, frequency);
+}
--- database/database.h
+++ database/database.h
@@ -3,11 +3,13 @@
 
 #include <pqxx/pqxx>
 #include "../config/config.h"
-#include <string>
 
 class Database {
 public:
     Database(const Config& config);
+
+    void save_document(const std::string& url, const std::string& content, pqxx::work& txn);
+    void save_word_frequency(int document_id, const std::string& word, int frequency, pqxx::work& txn);
 
     pqxx::connection& conn() { return conn_; }
 
